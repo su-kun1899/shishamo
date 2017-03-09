20466bb9f7674aa89318bb5de0f3f7f46f98cfd8
su-kun1899 <higedrum.coz@gmail.com>
2017-03-09 14:11:38 +0900

    Add switching enable embedded mysql

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ Mysql Metadata Visualizer
 
 Defalut branch:   
 * [![CircleCI](https://circleci.com/gh/su-kun1899/mysql-visualizer/tree/master.svg?style=svg)](https://circleci.com/gh/su-kun1899/mysql-visualizer/tree/master)
+
+## Embedded MySql
+
+You can use embedded mysql server for demo, testing, development.
+
+Like this:  
+- `java -jar mysql-visualizer.jar --mysqlvisualizer.embedded.mysql=true`
+- `./mvnw spring-boot:run -Drun.arguments="--mysqlvisualizer.embedded.mysql=true"`  
+
+Also you can custormize the configuration by `src/main/resources/embedded-mysql.yml`
--- src/main/java/red/sukun1899/MysqlVisualizerApplication.java
+++ src/main/java/red/sukun1899/MysqlVisualizerApplication.java
@@ -4,13 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import red.sukun1899.embedded.mysql.EmbeddedMySqlUtil;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class MysqlVisualizerApplication {
 
 	public static void main(String[] args) {
-		// TODO Sampleの時だけ動かしたい
-		EmbeddedMySqlUtil.start(EmbeddedMySqlUtil.loadConfiguration());
+		if (enableEmbeddedMysql(args)) {
+			EmbeddedMySqlUtil.start(EmbeddedMySqlUtil.loadConfiguration());
+		}
 
 		SpringApplication.run(MysqlVisualizerApplication.class, args);
+	}
+
+	static boolean enableEmbeddedMysql(String[] args) {
+		return Arrays.stream(args)
+				.filter(s -> s.equals("--mysqlvisualizer.embedded.mysql=true"))
+				.count() == 1;
 	}
 }
