55798defee1244a0eb930ca37a2d6808ca4a2a88
su-kun1899 <higedrum.coz@gmail.com>
2017-08-16 21:08:00 +0900

    Use postgresql database

--- src/main/resources/db/postgres/V0_0_1__init.sql
+++ src/main/resources/db/postgres/V0_0_1__init.sql
@@ -1,6 +1,11 @@
-CREATE TABLE `book` (
-  `isbn` bigint(19) NOT NULL COMMENT 'ISBN',
-  `title` varchar(128) NOT NULL COMMENT 'タイトル',
-  `publisherid` int(10) unsigned NOT NULL COMMENT '出版社ID',
-  PRIMARY KEY (`isbn`)
-) COMMENT='書籍';
+CREATE TABLE book (
+  isbn bigint NOT NULL,
+  title varchar(128) NOT NULL,
+  publisherid int NOT NULL,
+  PRIMARY KEY (isbn)
+);
+
+COMMENT ON TABLE book IS '書籍';
+COMMENT ON COLUMN book.isbn IS 'ISBN';
+COMMENT ON COLUMN book.title IS 'タイトル';
+COMMENT ON COLUMN book.publisherid IS '書籍';
