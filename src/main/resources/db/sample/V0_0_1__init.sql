fb470024c7ac45421c4fb422252b401333b0ef7d
su-kun1899 <higedrum.coz@gmail.com>
2017-11-22 16:01:08 +0900

    remove old sql

--- src/main/resources/db/sample/V0_0_1__init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `book` (
-  `book_id` int(10) unsigned NOT NULL COMMENT '書籍ID',
-  `isbn` bigint(19) UNIQUE NOT NULL COMMENT 'ISBN',
-  `title` varchar(128) NOT NULL COMMENT 'タイトル',
-  `publisher_id` int(10) unsigned NOT NULL COMMENT '出版社ID',
-  PRIMARY KEY (`book_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='書籍';
--- src/main/resources/db/sample/V0_1_0__add_member.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `member` (
-  `member_id` int(10) UNSIGNED NOT NULL COMMENT '会員ID',
-  `name` varchar(128) NOT NULL COMMENT '氏名',
-  PRIMARY KEY (`member_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='会員';
