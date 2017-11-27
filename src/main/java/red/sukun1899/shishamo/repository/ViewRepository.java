10f7589e9870cd39afe267233541e304f2849ae0
su-kun1899 <higedrum.coz@gmail.com>
2017-11-27 18:37:12 +0900

    add getting view

---
+++ src/main/java/red/sukun1899/shishamo/repository/ViewRepository.java
@@ -0,0 +1,16 @@
+package red.sukun1899.shishamo.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+import red.sukun1899.shishamo.model.View;
+
+import java.util.List;
+
+/**
+ * @author su-kun1899
+ */
+@Repository
+@Mapper
+public interface ViewRepository {
+    List<View> selectAll(String schemaName);
+}
---
+++ src/test/groovy/red/sukun1899/shishamo/repository/ViewRepositorySpec.groovy
@@ -0,0 +1,97 @@
+package red.sukun1899.shishamo.repository
+
+import com.ninja_squad.dbsetup.DbSetup
+import com.ninja_squad.dbsetup.destination.DataSourceDestination
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
+import org.springframework.boot.test.context.SpringBootTest
+import red.sukun1899.shishamo.embedded.mysql.EmbeddedMySqlUtil
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static com.ninja_squad.dbsetup.Operations.*
+
+/**
+ * @author su-kun1899
+ */
+@Unroll
+@SpringBootTest
+class ViewRepositorySpec extends Specification {
+    @Autowired
+    ViewRepository viewRepository
+
+    @Autowired
+    DataSourceProperties dataSourceProperties
+
+    @Autowired
+    DataSourceDestination destination
+
+    def setupSpec() {
+        if (EmbeddedMySqlUtil.enable()) {
+            EmbeddedMySqlUtil.ready()
+        }
+    }
+
+    def 'Get views'() {
+        given: 'Prepare test tables.'
+        new DbSetup(destination, sequenceOf(
+                sql('SET foreign_key_checks = 0'),
+                sql('DROP TABLE IF EXISTS book'),
+                sql('CREATE TABLE `book` (' +
+                        '  `isbn` bigint(19) NOT NULL COMMENT \'ISBN\',' +
+                        '  `title` varchar(128) NOT NULL COMMENT \'タイトル\',' +
+                        '  `publisher_id` int(10) unsigned NOT NULL COMMENT \'出版社ID\',' +
+                        '  PRIMARY KEY (`isbn`)' +
+                        ') ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT=\'書籍\''),
+                insertInto('book')
+                        .columns("isbn", "title", "publisher_id")
+                        .values(123, "FRA", 1)
+                        .values(456, "USA", 2)
+                        .values(789, "ENG", 3)
+                        .build(),
+                sql('SET foreign_key_checks = 1')
+        )).launch()
+
+        and: 'create view'
+        new DbSetup(destination, sequenceOf(
+                sql('SET foreign_key_checks = 0'),
+                sql('CREATE VIEW `view_book1` AS ' +
+                        'SELECT ' +
+                        '   `isbn` as `number`, ' +
+                        '   `title` as `name` ' +
+                        'FROM ' +
+                        '   `book`' +
+                        'WHERE' +
+                        '   `publisher_id` > 1'),
+                sql('CREATE VIEW `view_book2` AS ' +
+                        'SELECT ' +
+                        '   `isbn` as `number`, ' +
+                        '   `publisher_id` as `pub` ' +
+                        'FROM ' +
+                        '   `book`' +
+                        'WHERE' +
+                        '   `isbn` < 200'),
+                sql('SET foreign_key_checks = 1')
+        )).launch()
+
+        when:
+        def views = viewRepository.selectAll(dataSourceProperties.getSchema())
+
+        then:
+        views.size() == 2
+//        tables.each {
+//            assert it.getName() == 'book'
+//            assert it.getComment() == '書籍'
+//            assert it.getRowCount() == 2L
+//        }
+
+        cleanup:
+        new DbSetup(destination, sequenceOf(
+                sql('SET foreign_key_checks = 0'),
+                sql('DROP TABLE IF EXISTS `book`'),
+                sql('DROP VIEW IF EXISTS `view_book1`, `view_book2`'),
+                sql('SET foreign_key_checks = 1')
+        )).launch()
+    }
+
+}
