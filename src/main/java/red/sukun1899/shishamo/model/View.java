3ee5fefae31dc4a2ac9d6f68b731fa5375da12d7
su-kun1899 <higedrum.coz@gmail.com>
2017-11-28 17:58:38 +0900

    add fields

--- src/main/java/red/sukun1899/shishamo/model/View.java
+++ src/main/java/red/sukun1899/shishamo/model/View.java
@@ -1,10 +1,14 @@
 package red.sukun1899.shishamo.model;
 
+import java.util.List;
+
 /**
  * @author su-kun1899
  */
 public class View {
     private String name;
+    private Long rowCount;
+    private List<Column> columns;
 
     public String getName() {
         return name;
@@ -12,5 +16,21 @@ public class View {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(Long rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public List<Column> getColumns() {
+        return columns;
+    }
+
+    public void setColumns(List<Column> columns) {
+        this.columns = columns;
     }
 }
