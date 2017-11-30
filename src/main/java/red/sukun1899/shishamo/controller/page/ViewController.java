dbc1da925f6a5f38016eb82e11118e9b89e10ed2
su-kun1899 <higedrum.coz@gmail.com>
2017-11-30 18:33:15 +0900

    add view controller

---
+++ src/main/java/red/sukun1899/shishamo/controller/page/ViewController.java
@@ -0,0 +1,60 @@
+package red.sukun1899.shishamo.controller.page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import red.sukun1899.shishamo.model.Column;
+import red.sukun1899.shishamo.model.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author su-kun1899
+ */
+@Controller
+@RequestMapping("/views")
+public class ViewController {
+
+    @GetMapping
+    public String get(Model model) {
+        List<View> views = new ArrayList<View>() {
+            {
+                add(new View() {
+                    {
+                        setName("view1");
+                        setColumns(new ArrayList<Column>() {{
+                            add(new Column());
+                        }});
+                    }
+                });
+                add(new View() {
+                    {
+                        setName("view2");
+                        setColumns(new ArrayList<Column>() {{
+                            add(new Column());
+                            add(new Column());
+                        }});
+                    }
+                });
+                add(new View() {
+                    {
+                        setName("view3");
+                        setColumns(new ArrayList<Column>() {{
+                            add(new Column());
+                            add(new Column());
+                            add(new Column());
+                        }});
+                    }
+                });
+            }
+        };
+        model.addAttribute("views", views);
+
+        model.addAttribute("schemaName", "sample");
+
+        return "views";
+    }
+}
+
---
+++ src/test/groovy/red/sukun1899/shishamo/controller/page/ViewControllerSpec.groovy
@@ -0,0 +1,51 @@
+package red.sukun1899.shishamo.controller.page
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import red.sukun1899.shishamo.embedded.mysql.EmbeddedMySqlUtil
+import red.sukun1899.shishamo.model.Column
+import red.sukun1899.shishamo.model.View
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * @author su-kun1899
+ */
+@Unroll
+@SpringBootTest
+@AutoConfigureMockMvc
+class ViewControllerSpec extends Specification {
+    @Autowired
+    MockMvc mockMvc
+
+    def setupSpec() {
+        if (EmbeddedMySqlUtil.enable()) {
+            EmbeddedMySqlUtil.ready()
+        }
+    }
+
+    def 'Get all table list'() {
+        given:
+        def views = [
+                new View(name: 'view1', columns: [new Column()]),
+                new View(name: 'view2', columns: [new Column(), new Column()]),
+                new View(name: 'view3', columns: [new Column(), new Column(), new Column()]),
+        ]
+        and:
+        def url = '/views'
+
+        when:
+        def result = mockMvc.perform(MockMvcRequestBuilders.get(url))
+
+        then:
+        result.andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()))
+        result.andReturn().modelAndView.modelMap.get('views') != null
+        result.andReturn().modelAndView.modelMap.get('schemaName') == 'sample'
+    }
+
+}
