59921f78c74af3be5378fa94a73af9b421d36246
su-kun1899 <higedrum.coz@gmail.com>
2017-12-01 11:29:58 +0900

    add view service

---
+++ src/main/java/red/sukun1899/shishamo/service/ViewService.java
@@ -0,0 +1,26 @@
+package red.sukun1899.shishamo.service;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.stereotype.Service;
+import red.sukun1899.shishamo.model.View;
+import red.sukun1899.shishamo.repository.ViewRepository;
+
+import java.util.List;
+
+/**
+ * @author su-kun1899
+ */
+@Service
+public class ViewService {
+    private final DataSourceProperties dataSourceProperties;
+    private final ViewRepository viewRepository;
+
+    public ViewService(DataSourceProperties dataSourceProperties, ViewRepository viewRepository) {
+        this.dataSourceProperties = dataSourceProperties;
+        this.viewRepository = viewRepository;
+    }
+
+    public List<View> getAll() {
+        return viewRepository.select(dataSourceProperties.getName(), new ViewRepository.Condition());
+    }
+}
---
+++ src/test/groovy/red/sukun1899/shishamo/service/ViewServiceSpec.groovy
@@ -0,0 +1,39 @@
+package red.sukun1899.shishamo.service
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
+import red.sukun1899.shishamo.model.View
+import red.sukun1899.shishamo.repository.ViewRepository
+import spock.lang.Specification
+import spock.lang.Unroll
+/**
+ * @author su-kun1899
+ */
+@Unroll
+class ViewServiceSpec extends Specification {
+    ViewService viewService
+    DataSourceProperties dataSourceProperties
+    ViewRepository viewRepository
+
+    def setup() {
+        dataSourceProperties = Mock()
+        viewRepository = Mock()
+        viewService = new ViewService(dataSourceProperties, viewRepository)
+    }
+
+    def 'Get all view list'() {
+        given: 'Mocking repository'
+        viewRepository.select(*_) >> {
+            [
+                    new View(name: 'view1'),
+                    new View(name: 'view2'),
+            ]
+        }
+
+        when:
+        def views = viewService.getAll()
+
+        then:
+        views.size() == 2
+        views.collect {it.name} == ['view1','view2']
+    }
+}
